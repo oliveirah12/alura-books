b319d8930a14f9dfb469de0f3cbf975945cb3a1b
Matheus_Oliveira_Cne <matheus@cnesistemas.com.br>
2025-02-17 17:31:19 -0300

    primeira Integração entre back e front

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,8 @@
 import Input from '../Input'
 import styled from 'styled-components'
-import { useState } from 'react'
-import { livros } from './DadosPesquisa'
+import { useEffect, useState } from 'react'
+import { getLivros } from '../../servicos/livros'
+
 
 
 
@@ -55,6 +56,16 @@ const ListaResultados = styled.div`
 
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    const [livros, setLivros] = useState([])
+
+    useEffect(() => {
+        fetchLivros()}, []
+    )
+
+    async function fetchLivros() {
+        const livrosDaAPI = await getLivros()
+        setLivros(livrosDaAPI)
+    }
 
     return (
         <PesquisaContainer>
